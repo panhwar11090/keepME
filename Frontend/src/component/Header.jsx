1099e04e7849f0cd3a56aa2dfb91f2a101397261
huziafa <huzaifaa190@gmail.com>
2024-03-04 09:58:17 +0500

    vercel.json file added

--- Frontend/src/component/Header.jsx
+++ Frontend/src/component/Header.jsx
@@ -11,16 +11,14 @@ function Header() {
   );
   const navigate = useNavigate()
   const handleLogout = () => {
-    // Clear token or perform any necessary logout actions
-    // For example, you can remove token from local storage
+    
     localStorage.removeItem('token');
 
-    // Navigate to login page
-    // eslint-disable-next-line react-hooks/rules-of-hooks
+    
     navigate('/login');
   };
   return (
-    <div className="bg-white ml-0 mt-0 px-10 py-2 shadow-md flex flex-row items-center" style={{width:"100%"}}>
+    <div className="bg-white ml-0 mt-0 px-10 py-2 shadow-md flex flex-row items-center " style={{width:"100%"}}>
       <div className="flex items-center">
         {logo}
         <h1 className="text-gray-700 text-xl font-semibold">Keep</h1>
